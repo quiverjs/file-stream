d916d4bde9b7af1eb347978057fac68df9409884
Soares Chen <soares.chen@gmail.com>
2014-02-16 04:30:39 +0800

    Added etag attribute to file streamable and refactor tests.

--- test/test.js
+++ test/test.js
@@ -13,24 +13,27 @@ var testFilePath = path.join(__dirname, 'test-file.txt')
 var testWritePath = path.join(__dirname, 'test-write.txt')
 var testTempPath = path.join(__dirname, 'test-temp.txt')
 
-var getOriginalContent = function(callback) {
-  fs.readFile(testFilePath, { encoding: 'utf8' }, callback)
-}
-
 describe('file stream test', function() {
-  it('file read stream test', function(callback) {
-    getOriginalContent(function(err, originalContent) {
+  var originalContent
+
+  before(function(callback) {
+    fs.readFile(testFilePath, { encoding: 'utf8' }, function(err, content) {
       if(err) return callback(err)
 
-      fileStreamLib.createFileReadStream(testFilePath, function(err, readStream) {
+      originalContent = content
+      callback()
+    })
+  })
+
+  it('file read stream test', function(callback) {
+    fileStreamLib.createFileReadStream(testFilePath, function(err, readStream) {
+      if(err) throw err
+
+      streamConvert.streamToText(readStream, function(err, content) {
         if(err) throw err
 
-        streamConvert.streamToText(readStream, function(err, content) {
-          if(err) throw err
-
-          should.equal(content, originalContent)
-          callback(null)
-        })
+        should.equal(content, originalContent)
+        callback(null)
       })
     })
   })
@@ -48,82 +51,70 @@ describe('file stream test', function() {
   })
 
   it('file byte stream test', function(callback) {
-    getOriginalContent(function(err, originalContent) {
-      if(err) return callback(err)
+    var start = 8
+    var end = 32
 
-      var start = 8
-      var end = 32
+    var originalSlice = originalContent.slice(start, end)
 
-      var originalSlice = originalContent.slice(start, end)
+    fileStreamLib.createByteRangeFileStream(testFilePath, start, end, 
+      function(err, readStream) {
+        if(err) return callback(err)
 
-      fileStreamLib.createByteRangeFileStream(testFilePath, start, end, 
-        function(err, readStream) {
+        streamConvert.streamToText(readStream, function(err, content) {
           if(err) return callback(err)
 
-          streamConvert.streamToText(readStream, function(err, content) {
-            if(err) return callback(err)
-
-            should.equal(content, originalSlice)
-            callback(null)
-          })
+          should.equal(content, originalSlice)
+          callback(null)
         })
-    })
+      })
   })
 
   it('file streamable test', function(callback) {
-    getOriginalContent(function(err, originalContent) {
+    var fileSize = originalContent.length
+
+    var start = 16
+    var end = 64
+
+    var originalSlice = originalContent.slice(start, end)
+
+    fileStreamLib.createFileStreamable(testFilePath, function(err, streamable) {
       if(err) return callback(err)
 
-      var fileSize = originalContent.length
+      should.exists(streamable.toStream)
+      should.exists(streamable.toByteRangeStream)
+      should.exists(streamable.toFilePath)
 
-      var start = 16
-      var end = 64
+      should.equal(streamable.reusable, true)
+      should.equal(streamable.toFilePath(), testFilePath)
+      should.equal(streamable.contentLength, fileSize)
 
-      var originalSlice = originalContent.slice(start, end)
-
-      fileStreamLib.createFileStreamable(testFilePath, function(err, streamable) {
-        if(err) return callback(err)
-
-        should.exists(streamable.toStream)
-        should.exists(streamable.toByteRangeStream)
-        should.exists(streamable.toFilePath)
-
-        should.equal(streamable.reusable, true)
-        should.equal(streamable.toFilePath(), testFilePath)
-        should.equal(streamable.contentLength, fileSize)
-
-        callback()
-      })
+      callback()
     })
   })
 
   it('streamable to file path test', function(callback) {
-    getOriginalContent(function(err, originalContent) {
+    var tempGen = function(callback) {
+      callback(null, testTempPath)
+    }
+
+    fileStreamLib.createFileReadStream(testFilePath, function(err, readStream) {
       if(err) return callback(err)
 
-      var tempGen = function(callback) {
-        callback(null, testTempPath)
+      var streamable = {
+        toStream: function(callback) {
+          callback(null, readStream)
+        }
       }
 
-      fileStreamLib.createFileReadStream(testFilePath, function(err, readStream) {
+      fileStreamLib.streamableToFilePath(streamable, tempGen, function(err, filePath) {
         if(err) return callback(err)
 
-        var streamable = {
-          toStream: function(callback) {
-            callback(null, readStream)
-          }
-        }
-
-        fileStreamLib.streamableToFilePath(streamable, tempGen, function(err, filePath) {
+        should.equal(filePath, testTempPath)
+        fs.readFile(testTempPath, { encoding: 'utf8' }, function(err, tempContent) {
           if(err) return callback(err)
 
-          should.equal(filePath, testTempPath)
-          fs.readFile(testTempPath, { encoding: 'utf8' }, function(err, tempContent) {
-            if(err) return callback(err)
-
-            should.equal(tempContent, originalContent)
-            callback()
-          })
+          should.equal(tempContent, originalContent)
+          callback()
         })
       })
     })
